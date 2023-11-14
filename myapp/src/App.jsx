5a6fa5c7781152e4ac77f73641c147c6ed28915b
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-11-13 22:52:22 -0300

    Datos de Articulo

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,13 +3,40 @@ import Header from './Components/Header/Header'
 import Card from './Components/Card/Card'
 import './App.css'
 
+const products = 
+  {
+    codigo: "GOW001",
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_790481-MLM32575781979_102019-F.jpg",
+    nombre: "Figura de Kratos",
+    descripcion: "Figura coleccionable de Kratos de 25cm",
+    precio: 49.99,
+    cantidad: 10
+  }/*,
+  {
+    codigo: "GOW002",
+    imagen: "https://th.bing.com/th/id/R.df063a61e3fcccc30f5eb75cd365c9e2?rik=Okv8jYpZ%2fmhxjQ&pid=ImgRaw&r=0",
+    nombre: "Libro de arte de God of War",
+    descripcion: "Libro de arte conceptual del videojuego God of War",
+    precio: 29.99,
+    cantidad: 5
+  },
+  {
+    codigo: "GOW003",
+    imagen: "https://th.bing.com/th/id/OIP.00TxBytSqX-AlobQEt-c1AHaG2?pid=ImgDet&rs=1",
+    nombre: "Remera de God of War",
+    descripcion: "Remera de God of War color negro",
+    precio: 19.99,
+    cantidad: 20
+  }*/
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header/>
-      <Card/>
+      <Card products = {products} />
     </>
   )
 }
--- myapp/src/Components/Card/Card.jsx
+++ myapp/src/Components/Card/Card.jsx
@@ -1,28 +1,26 @@
-import {Card, ListGroup} from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
 
 
-function KitchenSinkExample() {
+function KitchenSinkExample({ products }) {
+  console.log(products)
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-  );
+    
+      <Card className='mx-auto mt-5' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={products.imagen} />
+        <Card.Body>
+          <Card.Title>{products.nombre}</Card.Title>
+          <Card.Text>
+            {products.descripcion}
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>Codigo producto: {products.codigo} </ListGroup.Item>
+          <ListGroup.Item>Precio: ${products.precio} </ListGroup.Item>
+          <ListGroup.Item>Stock: {products.cantidad}</ListGroup.Item>
+        </ListGroup>
+      </Card>
+    )
+  ;
 }
 
 export default KitchenSinkExample;
