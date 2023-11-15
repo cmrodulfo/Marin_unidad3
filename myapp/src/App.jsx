86d49505fa735da97b6a3ce4d72169357abd22b6
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-11-14 21:02:06 -0300

    Detalle de Producto

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,34 +1,17 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
-import Card from './Components/Card/Card'
 import './App.css'
+import ProductDetail from './ProductDetail/ProductDetail'
 
-const productos = [
-  {
-    codigo: "GOW001",
-    imagen: "https://http2.mlstatic.com/D_NQ_NP_790481-MLM32575781979_102019-F.jpg",
-    nombre: "Figura de Kratos",
-    descripcion: "Figura coleccionable de Kratos de 25cm",
-    precio: 49.99,
-    cantidad: 10
-  },
-  {
-    codigo: "GOW002",
-    imagen: "https://th.bing.com/th/id/R.df063a61e3fcccc30f5eb75cd365c9e2?rik=Okv8jYpZ%2fmhxjQ&pid=ImgRaw&r=0",
-    nombre: "Libro de arte de God of War",
-    descripcion: "Libro de arte conceptual del videojuego God of War",
-    precio: 29.99,
-    cantidad: 5
-  },
-  {
-    codigo: "GOW003",
-    imagen: "https://th.bing.com/th/id/OIP.00TxBytSqX-AlobQEt-c1AHaG2?pid=ImgDet&rs=1",
-    nombre: "Remera de God of War",
-    descripcion: "Remera de God of War color negro",
-    precio: 19.99,
-    cantidad: 20
+const product = {
+    code: "GOW001",
+    image: "https://http2.mlstatic.com/D_NQ_NP_790481-MLM32575781979_102019-F.jpg",
+    name: "Figura de Kratos",
+    description: "Figura coleccionable de Kratos de 25cm",
+    price: 49.99,
+    stock: 10
   }
-]
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,7 +19,7 @@ function App() {
   return (
     <>
       <Header/>
-      {productos.map(producto=><Card imagen={producto.imagen} nombre={producto.nombre} descripcion={producto.descripcion} cantidad={producto.cantidad} precio={producto.precio} codigo={producto.codigo} /> )  }
+      <ProductDetail product={product} />
     </>
   )
 }
