4b6d71a5caf53be16f556f294cc854bf84f44052
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-11-19 12:18:52 -0300

    Actualizacion App y Product Detail

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,25 +2,26 @@ import { useState } from 'react'
 import Header from './Components/Header/Header'
 import './App.css'
 import ProductDetail from './ProductDetail/ProductDetail'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const product = {
-    code: "GOW001",
-    image: "https://http2.mlstatic.com/D_NQ_NP_790481-MLM32575781979_102019-F.jpg",
-    name: "Figura de Kratos",
-    description: "Figura coleccionable de Kratos de 25cm",
-    price: 49.99,
-    stock: 10
-  }
+  code: "GOW001",
+  image: "https://http2.mlstatic.com/D_NQ_NP_790481-MLM32575781979_102019-F.jpg",
+  name: "Figura de Kratos",
+  description: "Figura coleccionable de Kratos de 25cm",
+  price: 49.99,
+  stock: 10
+}
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Header/>
+    <ChakraProvider>
+      <Header />
       <ProductDetail product={product} />
-    </>
+    </ChakraProvider>
   )
 }
 
--- myapp/src/ProductDetail/ProductDetail.jsx
+++ myapp/src/ProductDetail/ProductDetail.jsx
@@ -1,79 +1,82 @@
 import React from 'react';
-import { Box, Image, Text, Heading, Flex, Badge, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Button } from '@chakra-ui/react';
+import { Container, Box, Image, Text, Heading, Flex, Badge, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Button } from '@chakra-ui/react';
+
 
 
 const ProductDetail = ({ product }) => {
- 
+
   const { image, name, description, code, stock, price } = product;
 
-  
+
   const [quantity, setQuantity] = React.useState(1);
 
-  
+
   const handleChange = (value) => {
-    
+
     if (value > 0 && value <= stock) {
-    
+
       setQuantity(value);
     }
   };
 
-  
+
   return (
-   
-    <Box maxW="800px" mx="auto" p="4">
-     
-      <Flex direction={{ base: 'column', md: 'row' }} align="center">
-      
-        <Image src={image} alt={name} w='250px' margin='10px' />
-        
-        <Box ml={{ base: 0, md: '4' }} mt={{ base: '4', md: 0 }}>
-        
-          <Heading as="h1" size="lg" color="teal.500">
-            {name}
-          </Heading>
-          
-          <Text fontSize="lg" mt="2">
-            {description}
-          </Text>
-          
-          <Text fontSize="lg" mt="2" fontWeight="bold">
-            Código: {code}
-          </Text>
-         
-          <Badge colorScheme={stock > 0 ? 'green' : 'red'} mt="2">
-            Stock: {stock}
-          </Badge>
-          
-          <Text fontSize="lg" mt="2" color="gray.600">
-            Precio: ${price}
-          </Text>
-          
-          <NumberInput
-            value={quantity}
-            onChange={handleChange}
-            min={1}
-            max={stock}
-            mt="4"
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-         
-          <Button
-            colorScheme="teal"
-            size="lg"
-            mt="4"
-            disabled={stock === 0}
-          >
-            Comprar
-          </Button>
-        </Box>
-      </Flex>
-    </Box>
+    <Container maxW='container.sm' bg='green.400' color='#262626'>
+
+      <Box maxW="800px" mx="auto" p="4" bg="">
+
+        <Flex direction={{ base: 'column', md: 'row' }} align="center">
+
+          <Image src={image} alt={name} w='300px' margin='30px' />
+
+          <Box ml={{ base: 0, md: '4' }} mt={{ base: '4', md: 0 }}>
+
+            <Heading as="h1" size="lg" color="teal.500">
+              {name}
+            </Heading>
+
+            <Text fontSize="lg" mt="2">
+              {description}
+            </Text>
+
+            <Text fontSize="lg" mt="2" fontWeight="bold">
+              Código: {code}
+            </Text>
+
+            <Badge colorScheme={stock > 0 ? 'green' : 'red'} mt="2">
+              Stock: {stock}
+            </Badge>
+
+            <Text fontSize="lg" mt="2" color="gray.600">
+              Precio: ${price}
+            </Text>
+
+            <NumberInput
+              value={quantity}
+              onChange={handleChange}
+              min={1}
+              max={stock}
+              mt="4"
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+
+            <Button
+              colorScheme="teal"
+              size="lg"
+              mt="4"
+              disabled={stock === 0}
+            >
+              Comprar
+            </Button>
+          </Box>
+        </Flex>
+      </Box>
+    </Container>
   );
 };
 
